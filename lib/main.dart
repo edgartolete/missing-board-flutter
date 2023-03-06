3d9874be0280ed516572e1bfb46617197132a9e4
Edgar Tolete <edgar.tolete@volenday.com>
2023-03-06 13:00:10 +0800

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:app_project/question.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    title: 'Navigation Basics',
+    home: HomeApp(),
+  ));
+}
+
+class HomeApp extends StatefulWidget {
+  const HomeApp({Key? key}) : super(key: key);
+
+  @override
+  State<HomeApp> createState() => _HomeAppState();
+}
+
+class _HomeAppState extends State<HomeApp> {
+  var _questionIndex = 0;
+
+  void answerQuestion() {
+    setState(() {
+      _questionIndex = _questionIndex + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var questions = [
+      "What's your favorite color?",
+      "what's your favorite animal?"
+    ];
+
+    return MaterialApp(
+      initialRoute: '/',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('My App'),
+          backgroundColor: Colors.green,
+        ),
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Question(
+              questions[_questionIndex],
+            ),
+            ElevatedButton(
+              onPressed: answerQuestion,
+              child: const Text("Answer 1"),
+            ),
+            ElevatedButton(
+              onPressed: answerQuestion,
+              child: const Text("Answer 2"),
+            ),
+            ElevatedButton(
+              onPressed: answerQuestion,
+              child: const Text("Answer 3"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
